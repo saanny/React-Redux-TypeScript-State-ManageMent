1012093a3e8e73227613f4cad1cd6c19aac40527
amir ahmadi <rzone845t@gmail.com>
2021-02-23 18:38:01 +0330

    Add typeselector

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import { useActions } from "../hooks/useAction";
-import { useSelector } from "react-redux";
+import { useSelector } from "../hooks/useTypeSelector";
 export const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
-  const { data, error, loading } = useSelector(
-    (state: any) => state.repositories
-  );
+  const { data, error, loading } = useSelector((state) => state.repositories);
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     searchRepositories(term);
